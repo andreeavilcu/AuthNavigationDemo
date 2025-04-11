79fa9ca1b1c2f50256debef4154e61139bbe095e
andreeavilcu <andreea.vilcu@student.unitbv.ro>
2025-04-11 13:27:21 +0300

    Implemented XML layout files: -fragment_login.xml -fragment_profile.xml -fragment_register_step1.xml -fragment_register_step2.xml These layouts support the authentication and user profile flow.

--- app/src/main/java/com/example/androidauthnavigationdemo/MainActivity.kt
+++ app/src/main/java/com/example/androidauthnavigationdemo/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.androidauthnavigationdemo
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
 
 
     }
